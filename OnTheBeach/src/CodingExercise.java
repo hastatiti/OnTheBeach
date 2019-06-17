970687f81a64801f4dea9fbb945e97bc754536ab
ADY <devrimytr@yahoo.com>
2019-06-17 11:13:30 +0100

    Test console input, seperate line by line

--- OnTheBeach/src/CodingExercise.java
+++ OnTheBeach/src/CodingExercise.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class CodingExercise {
 	String s = "a => b => c =>";
 	String s2 = "a => b => c c =>";
+
 	public static ArrayList sequence(String job) {
 		List<String> myList = new ArrayList<>();
 
@@ -13,13 +14,21 @@ public class CodingExercise {
 	}
 
 	public static void main(String[] args) {
+		List<String> myList = new ArrayList();
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Entries : ");
-		while(sc.hasNextLine()) {
+		while (sc.hasNextLine()) {
 			String rawLine = sc.nextLine();
+			//get only letters line by line
 			String[] line = rawLine.split(" => ");
-			String firstInput = line[0];
-			String secondInput = line[1];
+				String firstInput = line[0];
+				String secondInput = line[1];
+				
+				//string concatenation in form of xy
+				String finalInput = firstInput + secondInput;
+				myList.add(finalInput);
 		}
+		System.out.println(myList);
 	}
 }
